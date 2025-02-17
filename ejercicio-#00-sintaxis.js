833249ebe1fd6a96970e3dbd9216ecd67ae95307
FranGarello <fgarello9@gmail.com>
2025-02-17 09:42:11 -0300

    agrego primeros ejercicios logicos simples de javascript

---
+++ ejercicio-#00-sintaxis.js
@@ -0,0 +1,25 @@
+// URL = https://nodejs.org/es
+
+// Tenemos dos formas de comentarios en Javascript:
+// Comentario de una linea
+
+/*
+   Comentario
+   Multilinea
+*/
+
+// Formas de crear una variable:
+let variable1 = null;
+const variable2 = null;
+var variable3 = null; 
+
+// Datos primitivos:
+let string = "string";
+let number = 1;
+let boolean = true;
+let bgint = 1231321155151115n;
+let bgint2 = BigInt(12341241124124);
+let symbol = Symbol("foo");
+
+// Impresion por consola
+console.log("¡Hola, Javascript!")
---
+++ ejercicio-#01-operadores.js
@@ -0,0 +1,57 @@
+// Operadores en javascript:
+
+// Aritmeticos:
+// Operador de suma (+):
+let num1 = 10;
+let num2 = 12;
+let result1 = num1 + num2;
+console.log("forma 1 de sumar: ", result1);
+console.log("forma 2 de sumar: ", num1 + num2);
+console.log("forma 3 de sumar: ", num1 + 15);
+
+// Operador de sustraccion (-):
+num1 = 15;
+num2 = 10;
+let result2 = num1 - num2;
+console.log("resta: ", result2);
+
+// Operador de multiplicacion (*):
+num1 = 4;
+num2 = 10;
+let result3 = num1 * num2;
+console.log("multiplicacion: ", result3);
+
+// Operador de division (/):
+num1 = 40;
+num2 = 10;
+let result4 = num1 / num2;
+console.log("division: ", result4);
+
+
+// Operador de modulo (%):
+/*
+   Este operador puede ser dificil de entender.
+   En la informatica el modulo es el operador utilizado
+   para obtener el resto de una division entre 2 numeros.
+*/
+num1 = 50;
+num2 = 3;
+let result5 = num1 % num2;
+console.log("modulo o resto de la division: ", result5);
+
+// Operador de exponenciacion (**):
+num1 = 3;
+console.log("exponenciancion: ", num1 ** 3);
+
+// Operador de incremento (++):
+num1 = 50;
+console.log("incremento: ", num1++);
+
+// Operador de decremento (--):
+num1 = 30;
+console.log("decremento: ", num1--);
+
+//--------------------------------
+
+// Operadores de asignacion:
+
